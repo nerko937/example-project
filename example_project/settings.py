6f8b9f2a5257bbf4b2e588971efdae8eaa2973cf
Marcin Fudala <nerko937@gmail.com>
2021-03-03 10:15:55 +0100

    added postgres db

--- example_project/settings.py
+++ example_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'example_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
