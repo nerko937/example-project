18d0f786903b4e75eb7432897b7d14ff9730c266
Marcin Fudala <nerko937@gmail.com>
2021-03-08 13:37:06 +0100

    fixup! added projects

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class Project(models.Model):
     name = models.CharField(max_length=256, unique=True)
     owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='project_owning')
-    creation_date = models.DateField(auto_now_add=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
     users = models.ManyToManyField(get_user_model(), related_name='projects')
 
     def __str__(self):
