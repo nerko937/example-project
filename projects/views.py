8d7cef7c09abf04f92165c42245e095dbbdd9773
Marcin Fudala <nerko937@gmail.com>
2021-03-08 12:47:59 +0100

    fixup! added projects

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,15 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 from .models import Project
 from .serializers import ProjectSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsOwnerOrReadOnly,)
     http_method_names = ('head', 'get', 'post', 'put', 'patch')
 
     def get_queryset(self):
         return Project.objects.filter(users=self.request.user)
+
