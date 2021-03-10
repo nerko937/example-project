8bd97ece002ec43f5523c59b4a292c856ae0f97c
Marcin Fudala <nerko937@gmail.com>
2021-03-10 12:27:15 +0100

    fixup! added registration

--- users/serializers.py
+++ users/serializers.py
@@ -1,14 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=128, write_only=True)
-
-    def validate_password(self, value):
-        validate_password(value)
-        return value
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
