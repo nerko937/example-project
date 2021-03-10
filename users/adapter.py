0f2aa43f43255c4dc1d0228c47c48658ef57bdc1
Marcin Fudala <nerko937@gmail.com>
2021-03-10 10:51:37 +0100

    fixup! added social authentication

--- users/adapter.py
+++ users/adapter.py
@@ -4,6 +4,7 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def new_user(self, request, sociallogin):
-        user = get_user_model()()
-        user.is_active = True
-        return user
+        User = get_user_model()
+        u = User()
+        u.is_active = True
+        return u
