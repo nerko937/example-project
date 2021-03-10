100201041ebfa46d0b10f0c46f4693a7057184be
Marcin Fudala <nerko937@gmail.com>
2021-03-10 09:40:39 +0100

    email notifications about issues

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from model_utils import FieldTracker
+
+from example_project.celery import app
+from .utils import email_if_assignee_changed
 
 
 class Project(models.Model):
@@ -32,6 +36,26 @@ class Issue(models.Model):
     owner = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='issues_owning')
     assignee = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='issues')
     status = models.CharField(choices=STATUS_CHOICES, max_length=8, default=TODO)
+    task_id = models.UUIDField(null=True, blank=True)
+    tracker = FieldTracker(fields=['due_date', 'assignee'])
 
     def __str__(self):
         return self.title
+
+    def notify_when_due_date_passed(self):
+        from .tasks import email_when_due_date_passed
+
+        t = email_when_due_date_passed.apply_async((self.pk,), eta=self.due_date)
+        self.task_id = t.task_id
+        self.save(update_fields=['task_id'])
+
+    def save(self, *args, **kwargs):
+        is_new = not self.pk
+        due_date_changed = self.tracker.has_changed('due_date')
+        email_if_assignee_changed(self)
+        super().save(*args, **kwargs)
+        if is_new:
+            self.notify_when_due_date_passed()
+        elif due_date_changed:
+            app.control.revoke(self.task_id, terminate=True)
+            self.notify_when_due_date_passed()
