5b2087fcee660b08a1418f1b8b5b21a9836239b4
Marcin Fudala <nerko937@gmail.com>
2021-03-11 12:29:52 +0100

    added missing id field in serializers

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,7 +5,7 @@ from .models import Project, Issue
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('name', 'owner', 'creation_date', 'users')
+        fields = ('id', 'name', 'owner', 'creation_date', 'users')
         model = Project
 
 
@@ -13,6 +13,16 @@ class IssueSerializer(serializers.ModelSerializer):
     owner = serializers.PrimaryKeyRelatedField(default=serializers.CurrentUserDefault(), read_only=True)
 
     class Meta:
-        fields = ('title', 'description', 'project', 'created_date', 'due_date', 'owner', 'assignee', 'status')
+        fields = (
+            'id',
+            'title',
+            'description',
+            'project',
+            'created_date',
+            'due_date',
+            'owner',
+            'assignee',
+            'status'
+        )
         read_only_fields = ('owner', 'created_date')
         model = Issue
